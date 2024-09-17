76c76a16f4e0203f56fcf94979bc0ea253d279f9
Michael Lambert <mlambert125@live.com>
2024-09-16 20:54:42 -0400

    changed to hash history

--- todo/src/router/index.js
+++ todo/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory('/todo/dist/'),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
